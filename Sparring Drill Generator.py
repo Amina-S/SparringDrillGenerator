3687ab36808d3dd2dccd5fa0e36beeb67a0432dc
Amina <amshikhalieva@gmail.com>
2019-07-22 22:32:51 -0500

    Fixed timing bug

--- Sparring Drill Generator.py
+++ Sparring Drill Generator.py
@@ -6,24 +6,28 @@ class Generator():
 
     def __init__(self, master):
         master.geometry("1000x600")
-        master.message = ttk.Label(master, font = ('Courier', 120, 'bold'), justify = CENTER, foreground = "dark green")
+        master.config(background = "light yellow")
+        master.message = ttk.Label(master, font = ('Courier', 140, 'bold'), justify = CENTER, foreground = "dark green", background = "light yellow")
         master.message.pack()
-        
+        master.update()
         for i in range (50):
-            drill = randint(1, 8)
-            print(drill)
-            if (randint(1,10) == 9):
+            drill = randint(1, 8) 
+            if (randint(1,10) % 2 == 0):
+                master.message.config(text = 'switch')
                 master.update()
-                master.message.after(1000, master.message.config(text = 'switch'))                
+                master.message.after(1900)         
             if (drill == 1 or drill == 3):
+                master.message.config(text = drill)
                 master.update()
-                master.message.after(3500, master.message.config(text = drill))                
-            elif (drill == 2 or drill == 5):
+                master.message.after(3800)
+            elif (drill == 2 or drill == 5 or drill == 6):
+                master.message.config(text = drill)
                 master.update()
-                master.message.after(1500, master.message.config(text = drill))                
-            else:
+                master.message.after(2800)
+             else:
+                master.message.config(text = drill)
                 master.update()
-                master.message.after(5000, master.message.config(text = drill))                
+                master.message.after(3300)
             
 def main():
     root = Tk()
